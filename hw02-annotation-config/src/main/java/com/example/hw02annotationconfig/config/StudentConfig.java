700f2b68b696b92a08949615a6b495e15a80c603
DmitryS <stolypin.dmitry@gmail.com>
2024-06-28 08:38:34 +0300

    hw2 - Домашняя работа по теме "Конфигурирование Spring-boot"

--- hw02-annotation-config/src/main/java/com/example/hw02annotationconfig/config/StudentConfig.java
+++ hw02-annotation-config/src/main/java/com/example/hw02annotationconfig/config/StudentConfig.java
@@ -4,9 +4,9 @@ import com.example.hw02annotationconfig.service.IOService;
 import com.example.hw02annotationconfig.service.StudentService;
 import com.example.hw02annotationconfig.service.StudentServiceImpl;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Repository;
 
-@Configuration
+@Repository
 public class StudentConfig {
     @Bean
     public StudentService getStudentService(IOService ioService) {
