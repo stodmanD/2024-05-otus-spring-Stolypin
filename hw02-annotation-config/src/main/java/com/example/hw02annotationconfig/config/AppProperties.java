b2e9b41d24a71bdbe0d099e4eb6fc08fac604891
DmitryS <stolypin.dmitry@gmail.com>
2024-07-03 08:00:06 +0300

    hw2 - Домашняя работа по теме "Конфигурирование Spring"

--- hw02-annotation-config/src/main/java/com/example/hw02annotationconfig/config/AppProperties.java
+++ hw02-annotation-config/src/main/java/com/example/hw02annotationconfig/config/AppProperties.java
@@ -3,8 +3,9 @@ package com.example.hw02annotationconfig.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @Data
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
