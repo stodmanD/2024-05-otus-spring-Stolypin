dd9ff6587fb41a7c98f89955c17bef648a0af63d
DmitryS <stolypin.dmitry@gmail.com>
2024-07-22 09:29:21 +0300

    hw06 JPA

--- hw06-jpa/src/main/java/com/example/hw06jpa/exceptions/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.hw06jpa.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-
-}
