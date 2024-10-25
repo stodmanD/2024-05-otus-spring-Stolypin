238799dc733a6cea0d62357a1095e22fff30c51d
DmitryS <stolypin.dmitry@gmail.com>
2024-10-25 11:35:55 +0300

    hw16-Docker

--- hw16-Docker/src/main/resources/schema.sql
+++ hw16-Docker/src/main/resources/schema.sql
@@ -1,24 +1,25 @@
-create table authors
+
+create table IF NOT EXISTS authors
 (
     id        bigserial,
     full_name varchar(255),
     primary key (id)
 );
 
-create table genres
+create table IF NOT EXISTS genres
 (
     id   bigserial,
     name varchar(255),
     primary key (id)
 );
-create table comments
+create table IF NOT EXISTS comments
 (
     id      bigserial,
     text    varchar(255),
     book_id bigint,
     primary key (id)
 );
-create table books
+create table IF NOT EXISTS books
 (
     id         bigserial,
     title      varchar(255),
@@ -28,7 +29,7 @@ create table books
 );
 
 
-create table books_genres
+create table IF NOT EXISTS books_genres
 (
     book_id  bigint references books (id) on delete cascade,
     genre_id bigint references genres (id) on delete cascade,
